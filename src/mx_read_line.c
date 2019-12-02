650495aca6e7fa06ad8b066418ce532199f9eb76
Maksym Dannyk <mdannyk@e2r4p8.unit.ua>
2019-12-02 19:56:54 +0200

    I change mx_read_line

--- src/mx_read_line.c
+++ src/mx_read_line.c
@@ -1,80 +1,59 @@
 #include "libmx.h"
 
-static char * mx_returned (char **lineptr) {
-	mx_strdel(lineptr);
-	return NULL;
-}
-static int set_str(char **rem, char **lineptr, int delim) {
-	char *tmp;
-    int len;
-
-	if (*rem && mx_strchr(*rem, delim)) {
-		len = mx_get_char_index(*rem, delim);
-		*lineptr = mx_strnew(len);  
-        *lineptr = mx_strncpy(*lineptr, *rem, len);
-        tmp = mx_strdup(&((*rem)[len + 1]));
-        mx_strdel(rem);
-        *rem = tmp;
-		if (*rem[0] == '\0') 
-			mx_strdel(rem);
-    }
-	else {
-		len = mx_strlen(*rem);
-	(*rem) ? *lineptr = mx_strdup(*rem) : (*lineptr = mx_returned(lineptr));
-    mx_strdel(rem);  
+static int mx_count_bytes(char **data, char **lineptr, char delim, int fd) {
+	int len = 0;
+	
+	if (!data[fd])
+		data[fd] = mx_strnew(0);
+	while (data[fd][len] != delim && data[fd][len] != '\0')
+			len++;
+	if (data[fd][len] == delim) {
+		*lineptr = mx_strndup(data[fd], len);
+		char *tmp = mx_strdup(data[fd] + ++len);
+		mx_strdel(&data[fd]);
+		data[fd] = tmp;
+		if (data[fd][0] == '\0')
+			mx_strdel(&data[fd]);
+		return mx_strlen(*lineptr);
 	}
-	return (len);
-}
+	else if (data[fd][0] == '\0') {
+		*lineptr = mx_strdup(data[fd]);
+		mx_strdel(&data[fd]);
+	}
+	return -1;
+}	
 
-static int get_ret_value(char **rem, char **lineptr, char delim, int ret) {
-	if (ret < 0)
-    	return -2;
-	if (ret == 0) {
-    	set_str(rem, lineptr, delim);
-        return -1;
+static int mx_check_data(int nbytes, char **data, const int fd) {
+	if (nbytes == 0 && (!data[fd] || data[fd][0] == '\0'))
+		return -1;
+	return 0;
+}
+static int mx_read(char **data, const int fd, size_t buf_size, char delim) {
+	char buf[buf_size];
+	char *temp = NULL;
+	int nbytes = 0;
+
+	while ((nbytes = read(fd, buf, buf_size)) > 0) {
+    	buf[nbytes] = '\0';
+        temp = mx_strjoin(data[fd], buf);
+		free(data[fd]);
+        data[fd] = temp;
+        if (mx_strchr(buf, delim))
+        	break;
     }
-    else
-		return set_str(rem, lineptr, delim);
+	return nbytes;
 }
-
-int mx_read_line(char **lineptr, int buf_size, char delim, const int fd) {
-	static char *rem [256];
-	char buf[buf_size + 1];
-	char *tmp;
-	int ret = 0;
-
-	if (buf_size < 0 || fd < 0)
+int mx_read_line(char **lineptr, size_t buf_size, char delim, const int fd) {
+	static char *data[255];
+	int nbytes = 0;
+	if (fd < 0 && fd > 255)
 		return -2;
-	if ((rem[fd] && !mx_strchr(rem[fd], delim)) || !rem[fd])
-		while ((ret = read(fd, buf, buf_size)) > 0) {
-        	buf[ret] = '\0';
-			if (rem[fd] == NULL)
-				rem[fd] = mx_strdup(buf);
-    		else {
-    			tmp = mx_strjoin(rem[fd], buf);
-        		free(rem[fd]);
-        		rem[fd] = tmp;
-    		}
-    		if (rem[fd] && mx_strchr(rem[fd], delim))
-    			break;
-    }
-	if (ret != -1 && ret < buf_size)
-		ret = 0;
-    return get_ret_value(&rem[fd], lineptr, delim, ret);
-}
 
-int main() {
-          int fd = open("file", O_RDONLY);
-       //   int fd2 = open("file2", O_RDONLY);
-          char *str = NULL;
-          printf("%d:%s\n", mx_read_line(&str, 20, 'f', fd), str);
-          printf("%d:%s\n", mx_read_line(&str, 20, 'f', fd), str);
-          printf("%d:%s\n", mx_read_line(&str, 20, 'f', fd), str);
-    printf("%d: %s\n", mx_read_line(&str, 20, 'f', fd), str);
-printf("%d: %s\n", mx_read_line(&str, 20, 'f', fd), str);
-printf("%d: %s\n", mx_read_line(&str, 20, 'f', fd), str);
-printf("%d: %s\n", mx_read_line(&str, 20, 'f', fd), str);
-printf("%d: %s\n", mx_read_line(&str, 20, 'f', fd), str);
-         // printf("%d: %s\n", mx_read_line(&str, 35, 't', fd), str);
-       //   printf("%d: %s\n", mx_read_line(&str, 4, '.', fd2), str);
-  }
+	if (!data[fd])
+		data[fd] = mx_strnew(0);
+	
+	nbytes = mx_read(data, fd, buf_size, delim); 
+	if ((mx_check_data(nbytes, data, fd) == -1))
+			return -1;
+	return mx_count_bytes(data, lineptr, delim, fd);
+}
